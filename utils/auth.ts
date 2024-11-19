e6d2552aa2397257e79487dc6c2afaf4611174bc
jzh <11297332+jiaozh1@user.noreply.gitee.com>
2024-11-19 09:08:08 +0800

    initial test version

---
+++ utils/auth.ts
@@ -0,0 +1,83 @@
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { getServerSession, type User } from "next-auth";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+        try {
+          const response = await fetch(
+            `${process.env.API_BASE_URL}/auth/login`,
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                username: credentials.email,
+                password: credentials.password,
+              }),
+            }
+          );
+          if (!response.ok) {
+            return null;
+          }
+
+          const data = await response.json();
+          console.log(data);
+          if (data?.data?.token) {
+            return {
+              id: data.data.user.id.toString(),
+              // name: data.data.user.name,
+              // email: data.data.user.email,
+              token: data.data.token,
+              // user: data.data.user
+            };
+          }
+          return null;
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }: any) {
+      if (user) {
+        token.accessToken = user.token;
+        token.user = user.user;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.accessToken = token.accessToken;
+      session.user = token.user;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+
+export const getAuthSession = () => getServerSession(authOptions);
